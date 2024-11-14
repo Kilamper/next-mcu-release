c5714ebed813671a5ab2904f2b76af176d15ef44
Kilamper <kilamper@gmail.com>
2024-11-14 22:44:49 +0000

    Add favicon link to head template

--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?= $title ?>: La próxima <?= $type ?> de Marvel</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <meta name="description" content="La próxima película de Marvel">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css">
